6656efae84f46fb8a9388ec274dfe61ba4626f0f
ahsmha <ahmed.5wna0@aleeas.com>
2024-02-23 22:09:19 +0530

    [test]: unmarshalling

--- api/server.go
+++ api/server.go
@@ -27,5 +27,7 @@ func StartServer(services service.Services, config *utils.Config) {
 	fmt.Println("watcher running")
 	fmt.Println("watcher running")
 	fmt.Println("watcher running")
+	fmt.Println("watcher running")
+
 	r.Run(":8089")
 }
